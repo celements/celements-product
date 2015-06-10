aafcbbaad61d110c47043049e03e8f2dabb065b1
Marc Sladek <marc.sladek@synventis.com>
2015-06-10 14:58:33 +0200

    fixed AbstractManager.getManagees()

--- src/main/java/com/celements/product/AbstractManager.java
+++ src/main/java/com/celements/product/AbstractManager.java
@@ -21,24 +21,31 @@ public abstract class AbstractManager<K, T extends IManagee<K>> implements Initi
 
   private Map<Class<? extends K>, List<T>> map;
 
+  private List<T> getManageesFromMap(Class<? extends K> clazz) {
+    if (!map.containsKey(clazz)) {
+      map.put(clazz, new ArrayList<T>());
+    }
+    return map.get(clazz);
+  }
+
+  protected abstract Class<T> getManageeClass();
+
+  protected abstract boolean shouldBeUnique(Class<? extends K> clazz);
+
   @Override
   public void initialize() throws InitializationException {
     map = new HashMap<Class<? extends K>, List<T>>();
-    for (T managee : loadManagees()) {
-      for (Class<? extends K> clazz : managee.getSupportedClasses()) {
-        if (!uniqueCheck(clazz)) {
-          getManageesFromMap(clazz).add(managee);
-        } else {
-          getLogger().error("failed to initialize unique key '{}' for managee '{}'", 
-              clazz, managee);
+    try {
+      for (T managee : componentManager.lookupList(getManageeClass())) {
+        for (Class<? extends K> clazz : managee.getSupportedClasses()) {
+          if (!uniqueCheck(clazz)) {
+            getManageesFromMap(clazz).add(managee);
+          } else {
+            getLogger().error("failed to initialize unique key '{}' for managee '{}'", 
+                clazz, managee);
+          }
         }
       }
-    }
-  }
-
-  private List<T> loadManagees() throws InitializationException {
-    try {
-      return componentManager.lookupList(getManageeClass());
     } catch (ComponentLookupException cle) {
       throw new InitializationException("unable to look up managee class", cle);
     }
@@ -48,15 +55,6 @@ public abstract class AbstractManager<K, T extends IManagee<K>> implements Initi
     return shouldBeUnique(clazz) && (getManageesFromMap(clazz).size() == 1);
   }
 
-  private List<T> getManageesFromMap(Class<? extends K> clazz) {
-    if (!map.containsKey(clazz)) {
-      map.put(clazz, new ArrayList<T>());
-    }
-    return map.get(clazz);
-  }
-
-  protected abstract boolean shouldBeUnique(Class<? extends K> clazz);
-
   protected List<T> getManagees(Class<? extends K> clazz) {
     return getManagees(clazz, null);
   }
@@ -64,17 +62,9 @@ public abstract class AbstractManager<K, T extends IManagee<K>> implements Initi
   protected List<T> getManagees(Class<? extends K> clazz, Set<String> allowed) {
     List<T> ret = new ArrayList<T>();
     if (allowed == null) {
-      ret = getManageesFromMap(clazz);
-    } else if (allowed.size() == 1) {
-      String hint = allowed.iterator().next();
-      try {
-        ret.add(componentManager.lookup(getManageeClass(), hint));
-      } catch (ComponentLookupException cle) {
-        getLogger().warn("getManagees: failed getting class '{}' with hint '{}'", 
-            getManageeClass(), hint);
-      }          
-    } else if (allowed.size() > 1) {
-      for (T managee : getManagees(clazz)) {
+      ret.addAll(getManageesFromMap(clazz));
+    } else {
+      for (T managee : getManageesFromMap(clazz)) {
         if (allowed.contains(managee.getName())) {
           ret.add(managee);
         }
@@ -82,8 +72,6 @@ public abstract class AbstractManager<K, T extends IManagee<K>> implements Initi
     }
     return Collections.unmodifiableList(ret);
   }
-
-  protected abstract Class<T> getManageeClass();
 
   protected abstract Logger getLogger();
 
